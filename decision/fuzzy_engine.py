5ca2a2d77393e053528c54c6907ef6f6e1f93d05
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-11 16:37:54 +0000

    Updated fuzzy engine to accept predicted moisture

--- decision/fuzzy_engine.py
+++ decision/fuzzy_engine.py
@@ -6,19 +6,20 @@ from decision.rules import get_rules
 def run_fuzzy(env):
 
     # Variables
-    soil = ctrl.Antecedent(np.arange(0, 101, 1), 'soilMoisture')
+    current_moisture = ctrl.Antecedent(np.arange(0, 101, 1), 'soilMoisture')
+    predicted_moisture = ctrl.Consequent(np.arange(0, 101, 1), 'predicted_moisture')
     temp = ctrl.Antecedent(np.arange(0, 51, 1), 'temperature')
     humidity = ctrl.Antecedent(np.arange(0, 101, 1), 'humidity')
-    light = ctrl.Antecedent(np.arange(0, 1001, 1), 'lightIntensity')
+    light = ctrl.Antecedent(np.arange(0, 3000, 1), 'lightIntensity')
     rain = ctrl.Antecedent(np.arange(0, 101, 1), 'rainProbability')
 
     irrigation = ctrl.Consequent(np.arange(0, 101, 1), 'irrigation')
 
     # Membership
-    define_memberships(soil, temp, humidity, light, rain, irrigation)
+    define_memberships(current_moisture, predicted_moisture, temp, humidity, light, rain, irrigation)
 
     # Rules
-    rules = get_rules(soil, temp, humidity, light, rain, irrigation)
+    rules = get_rules(current_moisture, predicted_moisture, temp, humidity, light, rain, irrigation)
 
     # System
     system = ctrl.ControlSystem(rules)
@@ -26,6 +27,7 @@ def run_fuzzy(env):
 
     # Inputs
     sim.input['soilMoisture'] = env["soilMoisture"]
+    sim.input['predicted_moisture'] = env["predicted_moisture"]
     sim.input['temperature'] = env["temperature"]
     sim.input['humidity'] = env["humidity"]
     sim.input['lightIntensity'] = env["lightIntensity"]
