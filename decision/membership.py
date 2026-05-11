5443c65bc17b58d909c17a130a1c3495085f31a9
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-11 16:37:21 +0000

    Added predicted moisture as membership param

--- decision/membership.py
+++ decision/membership.py
@@ -1,12 +1,17 @@
 import skfuzzy as fuzz
 
-def define_memberships(soil, temp, humidity, light, rain, irrigation):
+def define_memberships(current_moisture, predicted_moisture, temp, humidity, light, rain, irrigation):
 
     # Soil input
-    soil['very_low'] = fuzz.trimf(soil.universe, [0, 0, 20])
-    soil['low'] = fuzz.trimf(soil.universe, [10, 30, 50])
-    soil['medium'] = fuzz.trimf(soil.universe, [40, 60, 80])
-    soil['high'] = fuzz.trimf(soil.universe, [70, 100, 100])
+    current_moisture['very_low'] = fuzz.trimf(current_moisture.universe, [0, 0, 25])
+    current_moisture['low'] = fuzz.trimf(current_moisture.universe, [15, 35, 50])
+    current_moisture['medium'] = fuzz.trimf(current_moisture.universe, [45, 65, 80])
+    current_moisture['high'] = fuzz.trimf(current_moisture.universe, [70, 100, 100])
+
+    # Prediction input
+    predicted_moisture['low'] = fuzz.trimf(predicted_moisture.universe, [0, 0, 35])
+    predicted_moisture['medium'] = fuzz.trimf(predicted_moisture.universe, [30, 50, 70])
+    predicted_moisture['high'] = fuzz.trimf(predicted_moisture.universe, [60, 100, 100])
 
     # Temperature input
     temp['low'] = fuzz.trimf(temp.universe, [0, 0, 20])
@@ -19,9 +24,9 @@ def define_memberships(soil, temp, humidity, light, rain, irrigation):
     humidity['high'] = fuzz.trimf(humidity.universe, [60, 100, 100])
 
     # Light input
-    light['low'] = fuzz.trimf(light.universe, [0, 0, 300])
-    light['medium'] = fuzz.trimf(light.universe, [200, 500, 800])
-    light['high'] = fuzz.trimf(light.universe, [700, 1000, 1000])
+    light['low'] = fuzz.trimf(light.universe, [0, 0, 800])
+    light['medium'] = fuzz.trimf(light.universe, [600, 1400, 2200])
+    light['high'] = fuzz.trimf(light.universe, [1800, 2868, 2868])
 
     # Rain input
     rain['low'] = fuzz.trimf(rain.universe, [0, 0, 40])
