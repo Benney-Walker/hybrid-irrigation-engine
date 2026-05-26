7e6b350e853437f681ad59ef1dc96bb6c5c81a21
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-26 20:18:14 +0000

    Updated endpoint to receive a list

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from input.validator import validate_input
 from prediction.predictor import predict_environment
 from decision.fuzzy_engine import run_fuzzy
@@ -8,12 +9,29 @@ app = FastAPI()
 
 
 @app.post("/predict")
-def predict(data: dict):
+def predict(payload: list):
+
     try:
-        validated = validate_input(data)
-        predicted = predict_environment(validated)
-        result = run_fuzzy(predicted)
-        return format_output(result, data)
+
+        validated_history = validate_input(payload)
+
+        # Returns ONLY current row combined with predicted moisture
+        predicted_environment = predict_environment(validated_history)
+
+        # FUZZY DECISION
+        command_percentage = run_fuzzy(predicted_environment)
+
+        # REASON PLACEHOLDER
+        reason = []
+
+        return format_output(
+            command_percentage,
+            predicted_environment,
+            reason
+        )
 
     except Exception as e:
-        return {"error": str(e)}
+
+        return {
+            "error": str(e)
+        }
